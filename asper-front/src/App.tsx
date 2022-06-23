1824f3f7dfd99141408546ec093db321acbff251
nidnogg <henriquevt98@gmail.com>
2022-06-23 04:58:57 -0300

    Added barebones

---
+++ asper-front/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ASP.net</h1>
+        <p>
+          Data do be displayed here
+        </p>      
+      </header>
+    </div>
+  )
+}
+
+export default App
