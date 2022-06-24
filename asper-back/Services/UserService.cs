d6afbbf2e0636f56213fbf2b4c0c7691704f2905
nidnogg <henriquevt98@gmail.com>
2022-06-23 23:38:30 -0300

    Service + Model done

--- asper-back/Services/UserService.cs
+++ asper-back/Services/UserService.cs
@@ -11,13 +11,14 @@ public static class UserService
     {
         Users = new List<User>
         {
-            new User { id = 1, name = "John Doe", isAdmin = false }
+            new User { id = 1, name = "John Doe", isAdmin = false },
             new User { id = 2, name = "Jane Doe", isAdmin = false }
-
         };
     }
 
     public static List<User> GetAll() => Users;
+
+    public static User? Get(int id) => Users.FirstOrDefault(p => p.id == id);
 
     public static void Add(User user) 
     {
@@ -25,6 +26,20 @@ public static class UserService
         Users.Add(user);
     }
 
+    public static void Delete(int id) 
+    {
+        var user = Get(id);
+        if(user is null) return;
+
+        Users.Remove(user);
+    }
+
+    public static void Update(User user)
+    {
+        var idx = Users.FindIndex(p => p.id == user.id);
+        if(idx == -1) return;
+        Users[idx] = user;
+    }
     // TO-DO add delete and update methods.
     // from https://docs.microsoft.com/en-us/learn/modules/build-web-api-aspnet-core/5-exercise-add-data-store
 
